27debdd64cbc180a5669e22ab91a94eb886b2645
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-11-18 10:31:36 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@204 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- coin/inst/experimental/bootstrap.R
+++ coin/inst/experimental/bootstrap.R
@@ -1,5 +1,21 @@
 
-mboot <- function(it, B = 1000) {
+
+### compute expectation and variance for observations with x != 0
+expectvarcontrast <- function(x, y, w) {
+
+    if (max(abs(colSums(x))) > sqrt(.Machine$double.eps))
+        stop(sQuote("x"), " does not define contrasts")
+
+    ev <- apply(x, 2, function(xx) {
+        coin:::expectvaronly(matrix(xx, ncol = 1), y, w * as.numeric(abs(xx) > 0))
+    })
+
+    list(E = sapply(ev, function(e) e$E), V = sapply(ev, function(e) e$V))
+}
+
+
+### bootstrap scaled and shifted test statistics
+mboot <- function(it, B = 1000, xcontrast = TRUE) {
 
     ### get transformation g(X) and influence function h(Y)
     xtrans <- coin:::get_xtrans(it)
@@ -14,14 +30,24 @@ mboot <- function(it, B = 1000) {
     storage.mode(bs) <- "double"
 
     ### null values (\lambda_0 and \tau_0)
-    lambda0 <- expectation(it)
-    tau0 <- variance(it)
+    if (!xcontrast) {
+        lambda0 <- expectation(it)
+        tau0 <- variance(it)
+    } else {
+        ### in case x defines contrasts (i.e. colSums(x) == 0) 
+        ### compute expectation and variance for x[x != 0] only
+        ### Otherwise, subset pivotality fails (I guess).
+        ev <- expectvarcontrast(xtrans, ytrans, weights)
+        lambda0 <- ev$E
+        tau0 <- ev$V
+    }
 
     ### for each bootstrap sample, calculate _multivariate_ linear statistic
     T <- matrix(0, nrow = length(statistic(it, "linear")), ncol = ncol(bs))
     for (i in 1:ncol(bs))
         ### multivariate linear statistic
-        T[,i] <- .Call("R_LinearStatistic", xtrans, ytrans, bs[,i], PACKAGE = "coin")
+        T[,i] <- .Call("R_LinearStatistic", xtrans, ytrans, bs[,i], 
+                       PACKAGE = "coin")
 
     ### compute Z matrix: bootstrap scaled and shifted test statistics
     ET <- rowMeans(T)
@@ -45,10 +71,13 @@ pval <- function(Z, Tobs, type = c("single-step", "step-down"),
     alternative <- match.arg(alternative)
     type <- match.arg(type)
     Tobs <- drop(Tobs)
+
+    ### standardized Z and Tobs with empirical mean and variance
     EZ <- rowMeans(Z)
     sdZ <- apply(Z, 1, sd)
     Z <- (Z - EZ)/sdZ
     Tobs <- (Tobs - EZ)/sdZ
+
     if (alternative == "two.sided") {
         Z <- abs(Z)
         Tobs <- abs(Tobs)
@@ -57,6 +86,7 @@ pval <- function(Z, Tobs, type = c("single-step", "step-down"),
         Z <- -Z
         Tobs <- -Tobs
     }
+
     if (type == "single-step")
         return(sapply(Tobs, function(x) mean(cmax(Z) > x)))
     if (type == "step-down")
@@ -64,10 +94,10 @@ pval <- function(Z, Tobs, type = c("single-step", "step-down"),
 }
 
 mt.bootstrap <- function(object, B = 1000, 
-                      type = c("single-step", "step-down")) {
+                      type = c("single-step", "step-down"), ...) {
 
     Tobs <- statistic(object, "linear")
-    Z <- mboot(object, B = B)
+    Z <- mboot(object, B = B, ...)
     pval(Z, Tobs, alternative = object@statistic@alternative)
 }
 
