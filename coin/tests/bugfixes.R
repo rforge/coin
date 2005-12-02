56096fd1a4b11fbf93ecfb9de90318304f018ca1
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2005-12-02 09:18:37 +0000

    test names of expectation and covariance
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@271 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- coin/tests/bugfixes.R
+++ coin/tests/bugfixes.R
@@ -88,3 +88,23 @@ mydf$x[1] <- NA
 s <- statistic(independence_test(x ~ y, data = mydf, weights = ~ w), "linear")
 stopifnot(s == 28)
 
+### two observations only
+mydf <- data.frame(x = 1:10, y = gl(2, 5))
+independence_test(y ~ x, data = mydf, subset = c(1, 6))
+independence_test(y ~ x, data = mydf, subset = c(1, 2))
+try(independence_test(y ~ x, data = mydf, subset = 1))
+
+### names of expectation and covariance
+YOY <- data.frame(length = c(46, 28, 46, 37, 32, 41, 42, 45, 38, 44,
+                             42, 60, 32, 42, 45, 58, 27, 51, 42, 52,
+                             38, 33, 26, 25, 28, 28, 26, 27, 27, 27,
+                             31, 30, 27, 29, 30, 25, 25, 24, 27, 30),
+                  site = factor(c(rep("I", 10), rep("II", 10),
+                                  rep("III", 10), rep("IV", 10))))
+
+it <- independence_test(length ~ site, data = YOY,
+    ytrafo = function(data) trafo(data, numeric_trafo = rank),
+    teststat = "quadtype")
+expectation(it)
+covariance(it)
+
