49c4d03583b354f5d6658fef02c38883d8dea8a1
hothorn <hothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-16 16:36:33 +0000

    1.0-2: confint_location fix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@570 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/coin/inst/ideas/RV.R
+++ trunk/coin/inst/ideas/RV.R
@@ -7,39 +7,18 @@ example(coeffRV)
 X <- as.matrix(X)
 Y <- as.matrix(Y)
 
-fm <- paste(paste(names(Y), collapse = "+"), "~", 
-            paste(names(X), collapse = "+"), collapse = "")
+fm <- paste(paste(colnames(Y), collapse = "+"), "~", 
+            paste(colnames(X), collapse = "+"), collapse = "")
 fm <- as.formula(fm)
 
 it <- independence_test(fm, data = wine, teststat = "quad")
 
-    coefficientRV <- function(X, Y) {
-        if (dim(X)[[1]] != dim(Y)[[1]]) 
-            stop("no the same dimension for X and Y")
-        if (dim(X)[[1]] == 1) {
-            print("1 configuration RV is  NA")
-            rv = NA
-        }
-        else {
-            Y <- scale(Y, scale = FALSE)
-            X <- scale(X, scale = FALSE)
-            W1 <- X %*% t(X)
-            W2 <- Y %*% t(Y)
-            rv <- sum(diag(W1 %*% W2))/(sum(diag(W1 %*% W1)) * 
-                sum(diag(W2 %*% W2)))^0.5
-        }
-        return(rv)
-    }
+x <- it@statistic@xtrans
+y <- it@statistic@ytrans
+w <- it@statistic@weights
+b <- as.integer(it@statistic@weights)
 
+mc <- coin:::MCfun(x, y, w, b, B = 20000)
+s1 <- apply(mc, 2, function(x) crossprod(x))
 
-p <- q <- 2
-n <- 100
-
-pval <- numeric(1000)
-for (i in 1:length(pval)) {
-    x <- as.data.frame(matrix(runif(n * p * q), nrow = n))
-    fm <- paste(paste(names(x)[1:p], collapse = "+"), "~", 
-                paste(names(x)[(p+1):(p + q)], collapse = "+") , collapse = "")
-    fm <- as.formula(fm)
-    pval[i] <- pvalue(independence_test(fm, data = x, teststat = "quad"))
-}
+mean(s1 > sum(statistic(it, "linear")^2))
