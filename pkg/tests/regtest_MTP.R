f1981eb07d398807c85e26dbbac3b441eac22fd9
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-28 15:05:03 +0000

    MTP updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@632 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/regtest_MTP.R
+++ pkg/tests/regtest_MTP.R
@@ -175,3 +175,25 @@ pss6 <- pvalue(it6, "single-step")
 psd6 <- pvalue(it6, "step-down")
 identical(pg6, min(pss6))
 identical(pg6, min(psd6))
+
+### mcp
+
+YOY <- data.frame(length = c(46, 28, 46, 37, 32, 41, 42, 45, 38, 44,
+                             42, 60, 32, 42, 45, 58, 27, 51, 42, 52,
+                             38, 33, 26, 25, 28, 28, 26, 27, 27, 27,
+                             31, 30, 27, 29, 30, 25, 25, 24, 27, 30),
+                  site = factor(c(rep("I", 10), rep("II", 10),
+                                  rep("III", 10), rep("IV", 10))))
+
+### permutation based Dunnett
+it <- independence_test(length ~ site, data = YOY,
+                        xtrafo = mcp_trafo(site = "Dunnett"),
+                        distribution = approximate(10000),
+                        alternative = "two.sided")
+pvalue(it, method = "npmcp")
+
+### asymptotic Dunnett
+it <- independence_test(length ~ site, data = YOY,
+                        xtrafo = mcp_trafo(site = "Dunnett"),
+                        alternative = "two.sided")
+pvalue(it, method = "npmcp")
