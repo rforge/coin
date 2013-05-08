2a10fd584838e291f8e96ff6bdb29d2e6c9bdcfe
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-08 14:54:51 +0000

    separate the paired case
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@758 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ExactDistributions.R
+++ pkg/R/ExactDistributions.R
@@ -15,55 +15,29 @@ SR_shift_2sample <- function(object, fact = NULL) {
 
     RET <- new("ExactNullDistribution")
 
-    ### this is indeed the one sample case
-    if (all(tabulate(object@block) == 2) && nlevels(object@block) > 1) {
-        scores <- object@ytrans[, 1]
-        ### search for equivalent integer scores with sum(scores) minimal
-        if (is.null(fact)) {
-            fact <- c(1, 2, 10, 100, 1000)
-            f <- is_integer(scores, fact = fact)
-            if (!any(f))
-                stop("cannot compute exact distribution with real valued scores")
-            fact <- min(fact[f])
-        }
-        ###  table(object$block, scores == 0) checken
-        sc <- round(scores * fact)
-        sc <- unlist(tapply(sc, object@block, function(x) {
-            if (any(x != 0)) return(x[x != 0])
-            return(0)
-        }))
-        storage.mode(sc) <- "integer"
-        Prob <- .Call("R_cpermdist1", sc, PACKAGE = "coin")
-        T <- which(Prob != 0)
-        Prob <- Prob[T]
-        ### 0 is possible
-        T <- (T - 1) / fact
-    } else {
+    ### in case we can't map the scores into integers, use another algorithm
+    if (!any(is_integer(object@ytrans[,1])))
+        return(vdW_split_up_2sample(object))
 
-        ### in case we can't map the scores into integers, use another algorithm
-        if (!any(is_integer(object@ytrans[,1])))
-            return(vdW_split_up_2sample(object))
+    T <- 0
+    Prob <- 1
+    for (lev in levels(object@block)) {
 
-        T <- 0
-        Prob <- 1
-        for (lev in levels(object@block)) {
+        thisblock <- (object@block == lev)
 
-            thisblock <- (object@block == lev)
+        ### compute distribution of scores in this block
+        scores <- object@ytrans[thisblock, 1]
+        m <- sum(object@xtrans[thisblock, 1] == 1)
 
-            ### compute distribution of scores in this block
-            scores <- object@ytrans[thisblock, 1]
-            m <- sum(object@xtrans[thisblock, 1] == 1)
+        if (m == 0) next;
+        if (m == length(scores))
+            dens <- list(T = sum(scores), Prob = 1)
+        if (m < length(scores))
+            dens <- cSR_shift_2sample(scores, m, fact = fact)
 
-            if (m == 0) next;
-            if (m == length(scores))
-                dens <- list(T = sum(scores), Prob = 1)
-            if (m < length(scores))
-                dens <- cSR_shift_2sample(scores, m, fact = fact)
-
-            ### update distribution of statistic over all blocks
-            T <- as.vector(outer(dens$T, T, "+"))
-            Prob <- drop(kronecker(Prob, dens$Prob))
-        }
+        ### update distribution of statistic over all blocks
+        T <- as.vector(outer(dens$T, T, "+"))
+        Prob <- drop(kronecker(Prob, dens$Prob))
     }
 
     T <- (T - expectation(object)) / sqrt(variance(object))
@@ -87,9 +61,73 @@ SR_shift_2sample <- function(object, fact = NULL) {
         )
     }
     RET@support <- function() T
-    RET@name <- "exact distribution (via Streitberg-Roehmel algorithm)"
+    RET@name <- "exact independent two-sample distribution (via Streitberg-Roehmel algorithm)"
     return(RET)
 }
+
+### Streitberg-Roehmel algorithm for paired samples
+SR_shift_1sample <- function(object, fact = NULL) {
+
+    if (!extends(class(object), "ScalarIndependenceTestStatistic"))
+        stop("Argument ", sQuote("object"), " is not of class ",
+             sQuote("ScalarIndependenceTestStatistic"))
+
+    if (!is_2sample(object)) 
+        stop(sQuote("object"), 
+             " does not represent an independent two-sample problem")
+
+    if (!(max(abs(object@weights - 1.0)) < eps()))
+        stop("cannot compute exact distribution with non-unity weights")
+
+    RET <- new("ExactNullDistribution")
+
+    scores <- object@ytrans[, 1]
+    ### search for equivalent integer scores with sum(scores) minimal
+    if (is.null(fact)) {
+        fact <- c(1, 2, 10, 100, 1000)
+        f <- is_integer(scores, fact = fact)
+        if (!any(f))
+            stop("cannot compute exact distribution with real valued scores")
+        fact <- min(fact[f])
+    }
+    ###  table(object$block, scores == 0) checken
+    sc <- round(scores * fact)
+    sc <- unlist(tapply(sc, object@block, function(x) {
+        if (any(x != 0)) return(x[x != 0])
+        return(0)
+    }))
+    storage.mode(sc) <- "integer"
+    Prob <- .Call("R_cpermdist1", sc, PACKAGE = "coin")
+    T <- which(Prob != 0)
+    Prob <- Prob[T]
+    ### 0 is possible
+    T <- (T - 1) / fact
+
+    T <- (T - expectation(object)) / sqrt(variance(object))
+
+    RET@p <- function(q) sum(Prob[LE(T, q)])
+    RET@q <- function(p) {
+        indx <- which(cumsum(Prob) < p)
+        if (length(indx) == 0) indx <- 0
+        T[max(indx) + 1]
+    }
+    RET@d <- function(x) Prob[T == x]
+    RET@pvalue <- function(q) {
+        switch(object@alternative, 
+            "less"      = sum(Prob[LE(T, q)]),
+            "greater"   = sum(Prob[GE(T, q)]),
+            "two.sided" = {
+                if (q == 0) return(1)
+                return(sum(Prob[LE(T, ifelse(q >  0, -q,  q))]) + 
+                       sum(Prob[GE(T, ifelse(q >= 0,  q, -q))]))
+            }
+        )
+    }
+    RET@support <- function() T
+    RET@name <- "exact paired two-sample distribution (via Streitberg-Roehmel algorithm)"
+    return(RET)
+}
+
 
 cSR_shift_2sample <- function(scores, m, fact = NULL) {
 
@@ -193,6 +231,6 @@ vdW_split_up_2sample <- function(object) {
         )
     }
     RET@support <- function(p = 1e-5) NA
-    RET@name <- "exact distribution (via van de Wiel split-up algorithm)"
+    RET@name <- "exact independent two-sample distribution (via van de Wiel split-up algorithm)"
     return(RET)
 }
