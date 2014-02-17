492e1246be4aab27593166b901807bd9017deafd
hnilsson <hnilsson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-17 16:13:44 +0000

    add checks demonstrating a bug in the S-R algorithm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@859 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/regtest_distribution.R
+++ pkg/tests/regtest_distribution.R
@@ -162,10 +162,10 @@ y3w <- with(dta, rep(y3, w))
 xw <- with(dta, rep(x, w))
 it1 <- independence_test(y3w ~ xw, distribution = exact(algorithm = "shift"))
 it2 <- independence_test(y3w ~ xw, distribution = exact(algorithm = "split-up"))
-isequal(pvalue(itw1), pvalue(it1))
-isequal(pvalue(itw1), pvalue(it2))
-isequal(pvalue(itw2), pvalue(it1))
-isequal(pvalue(itw2), pvalue(it2))
+stopifnot(isequal(pvalue(itw1), pvalue(it1)))
+stopifnot(isequal(pvalue(itw1), pvalue(it2)))
+stopifnot(isequal(pvalue(itw2), pvalue(it1)))
+stopifnot(isequal(pvalue(itw2), pvalue(it2)))
 
 Convictions <-
     matrix(c(2, 10, 15, 3), nrow = 2,
@@ -181,7 +181,80 @@ it1 <- independence_test(Var2 ~ Var1, alternative = "less",
 it2 <- independence_test(Var2 ~ Var1, alternative = "less",
                          data = coin:::table2df(as.table(Convictions)),
                          distribution = exact(algorithm = "split-up"))
-isequal(pvalue(itw1), pvalue(it1))
-isequal(pvalue(itw1), pvalue(it2))
-isequal(pvalue(itw2), pvalue(it1))
-isequal(pvalue(itw2), pvalue(it2))
+stopifnot(isequal(pvalue(itw1), pvalue(it1)))
+stopifnot(isequal(pvalue(itw1), pvalue(it2)))
+stopifnot(isequal(pvalue(itw2), pvalue(it1)))
+stopifnot(isequal(pvalue(itw2), pvalue(it2)))
+
+
+### check support, pperm, dperm, qperm,
+y1 <- c(1.83,  0.50,  1.62,  2.48, 1.68, 1.88, 1.55, 3.06, 1.30)
+y2 <- c(0.878, 0.647, 0.598, 2.05, 1.06, 1.29, 1.06, 3.14, 1.29)
+dta1 <- data.frame(
+    y = c(y1, y2),
+    x = gl(2, length(y1)),
+    b = factor(rep(seq_along(y1), 2)))
+
+diff <- y1 - y2
+dta2 <- data.frame(
+    y = as.vector(rbind(abs(diff) * (diff >= 0), abs(diff) * (diff < 0))),
+    x = factor(rep(0:1, length(diff)), labels = c("pos", "neg")),
+    b <- gl(length(diff), 2))
+
+### shift without block
+it1_SR <- independence_test(y ~ x, data = dta1,
+                            distribution = exact(algorithm = "shift"))
+supp_it1_SR <- support(it1_SR)
+stopifnot(!is.unsorted(supp_it1_SR))
+stopifnot(all(supp_it1_SR == unique(supp_it1_SR)))
+round(pp_it1_SR <- pperm(it1_SR, supp_it1_SR), 7)
+round(dp_it1_SR <- dperm(it1_SR, supp_it1_SR), 7)
+round(qp_it1_SR <- qperm(it1_SR, seq(0, 1, 0.01)), 7)
+
+### split-up without block
+it1_vdW <- independence_test(y ~ x, data = dta1,
+                             distribution = exact(algorithm = "split-up"))
+round(pp_it1_vdW <- pperm(it1_vdW, supp_it1_SR), 7)
+round(qp_it1_vdW <- qperm(it1_vdW, seq(0, 1, 0.01)), 7)
+
+### should be equal
+stopifnot(isequal(pp_it1_SR, pp_it1_vdW))
+stopifnot(isequal(qp_it1_SR[-c(1, 101)], qp_it1_vdW[-c(1, 101)]))
+stopifnot(isequal(pvalue(it1_SR), pvalue(it1_vdW)))
+
+### shift with block
+it2_SR <- independence_test(y ~ x | b, data = dta1,
+                            distribution = exact(algorithm = "shift"))
+supp_it2_SR <- support(it2_SR)
+### <FIXME>
+#stopifnot(!is.unsorted(supp_it2_SR))
+!is.unsorted(supp_it2_SR)
+#stopifnot(all(supp_it2_SR == unique(supp_it2_SR)))
+all(supp_it2_SR == unique(supp_it2_SR))
+### </FIXME>
+round(pp_it2_SR <- pperm(it2_SR, supp_it2_SR), 7)
+### <FIXME>
+#round(dp_it2_SR <- dperm(it2_SR, supp_it2_SR), 7)
+try(round(dp_it2_SR <- dperm(it2_SR, supp_it2_SR), 7))
+### </FIXME>
+round(qp_it2_SR <- qperm(it2_SR, seq(0, 1, 0.01)), 7)
+
+### paired shift with block
+it3_SR <- independence_test(y ~ x | b, data = dta2,
+                            distribution = exact(algorithm = "shift"),
+                            paired = TRUE)
+supp_it3_SR <- support(it3_SR)
+stopifnot(!is.unsorted(supp_it3_SR))
+stopifnot(all(supp_it3_SR == unique(supp_it3_SR)))
+round(pp_it3_SR <- pperm(it3_SR, supp_it3_SR), 7)
+round(dp_it3_SR <- dperm(it3_SR, supp_it3_SR), 7)
+round(qp_it3_SR <- qperm(it3_SR, seq(0, 1, 0.01)), 7)
+
+### should be equal
+### <FIXME>
+#stopifnot(isequal(pp_it2_SR, pp_it3_SR))
+isequal(pp_it2_SR, pp_it3_SR)
+#stopifnot(isequal(qp_it2_SR, qp_it3_SR))
+isequal(qp_it2_SR, qp_it3_SR)
+### </FIXME>
+stopifnot(isequal(pvalue(it2_SR), pvalue(it3_SR)))
