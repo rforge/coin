8b1d9a6ff65613d9811bb79d085b8469f3129735
hnilsson <hnilsson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-10-14 14:14:24 +0000

    move "ExpectCovar" and "ExpectCovarInfluence" to Classes.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@925 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Classes.R
+++ pkg/R/Classes.R
@@ -1,3 +1,21 @@
+### Conditional Expectation and Covariance
+setClass("ExpectCovar",
+    representation = representation(
+        expectation = "numeric",
+        covariance  = "matrix",
+        dimension   = "integer"
+   )
+)
+
+### Expectation and Covariance of the influence function
+### (+ sum of weights)
+setClass("ExpectCovarInfluence",
+    contains = "ExpectCovar",
+    representation = representation(
+        sumweights = "numeric"
+    )
+)
+
 ### Covariance matrix
 setClass("CovarianceMatrix",
     representation = representation(
