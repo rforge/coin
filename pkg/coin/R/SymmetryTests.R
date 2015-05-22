aedde8f8c1528b4bca52988ec86a4c2458af831d
hnilsson <hnilsson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-22 18:02:02 +0000

    cosmetics: reorganized tests in a more "logical" way
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@943 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/coin/R/SymmetryTests.R
+++ pkg/coin/R/SymmetryTests.R
@@ -1,85 +1,54 @@
-### Quade Test
-quade_test <- function(object, ...) UseMethod("quade_test")
+### sign test
+sign_test <- function(object, ...) UseMethod("sign_test")
 
-quade_test.formula <- function(formula, data = list(), subset = NULL, ...) {
-
-    ft("quade_test", "SymmetryProblem", formula, data, subset,
-       frame = parent.frame(), ...)
-}
-
-quade_test.SymmetryProblem <- function(object, ...) {
-
-    args <- setup_args(check = function(object) {
-                           if (!(is_Ksample(object) && is_numeric_y(object)))
-                               stop(sQuote("object"),
-                                    " does not represent a K-sample problem",
-                                    " (maybe the grouping variable is not a",
-                                    " factor?)")
-                           return(TRUE)
-                       })
-    ## convert factors to ordered and attach scores if requested
-    if (!is.null(args$scores)) {
-        object <- setscores(object, args$scores)
-        args$scores <- NULL
-    }
-    ## set test statistic to scalar for linear-by-linear tests
-    args$teststat <- if (is_ordered_x(object)) "scalar"
-                     else "quadratic"
-
-    y <- split(object@y[[1]], object@block)
-    R <- lapply(y, function(y) rank(y) - (length(y) + 1) / 2)
-    Q <- rank(vapply(y, function(y) max(y) - min(y), NA_real_, USE.NAMES = FALSE))
-    object@y[[1]] <- unsplit(lapply(seq_along(Q), function(i) Q[i] * R[[i]]),
-                             object@block)
-
-    object <- do.call("symmetry_test", c(list(object = object), args))
-
-    if (is_ordered_x(object@statistic))
-        object@method <- "Linear-by-Linear Association Test"
-    else
-        object@method <- "Quade Test"
-
+sign_test.formula <- function(formula, data = list(), subset = NULL, ...)
+{
+    object <- formula2data(formula, data, subset, frame = parent.frame(), ...)
+    if (is.null(object$bl))
+        object <- list(y = data.frame(c(object$y[[1]], object$x[[1]])),
+                       x = data.frame(gl(2, length(object$x[[1]]))),
+                       block = factor(rep.int(1:length(object$x[[1]]), 2)))
+    object <- new("SymmetryProblem", x = object$x, y = object$y,
+                  block = object$bl)
+    object <- do.call("sign_test", c(list(object = object), list(...)))
     return(object)
 }
 
+sign_test.SymmetryProblem <- function(object, ...) {
 
-### Friedman Test
-friedman_test <- function(object, ...) UseMethod("friedman_test")
+    y <- object@y[[1]]
+    x <- object@x[[1]]
 
-friedman_test.formula <- function(formula, data = list(), subset = NULL, ...) {
+    if (!is.numeric(y))
+        stop(sQuote("y"), " is not a numeric variable")
+    if (is.factor(x)) {
+        if (nlevels(x) != 2)
+            stop(sQuote("x"), " is not a factor with two levels")
+        diffs <- tapply(1:length(y), object@block, function(b)
+            y[b][x[b] == levels(x)[1]] - y[b][x[b] == levels(x)[2]])
+    } else
+        stop(sQuote("x"), " is not a factor")
 
-    ft("friedman_test", "SymmetryProblem", formula, data, subset,
-       frame = parent.frame(), ...)
-}
+    abs_diffs <- abs(diffs)
+    if (all(abs_diffs < .Machine$double.eps))
+        stop("all pairwise differences equal zero")
 
-friedman_test.SymmetryProblem <- function(object, ...) {
+    diffs <- diffs[abs_diffs > 0]
+    n <- length(diffs)
 
-    args <- setup_args(ytrafo = function(data)
-                           trafo(data, numeric_trafo = rank_trafo,
-                                 block = object@block),
-                       check = function(object) {
-                           if (!(is_Ksample(object) && is_numeric_y(object)))
-                               stop(sQuote("object"),
-                                    " does not represent a K-sample problem",
-                                    " (maybe the grouping variable is not a",
-                                    " factor?)")
-                           return(TRUE)
-                       })
-    ## convert factors to ordered and attach scores if requested
-    if (!is.null(args$scores)) {
-        object <- setscores(object, args$scores)
-        args$scores <- NULL
-    }
-    ## set test statistic to scalar for linear-by-linear tests
-    args$teststat <- if (is_ordered_x(object)) "scalar"
-                     else "quadratic"
+    object <- new("SymmetryProblem",
+                  x = data.frame(x = factor(rep.int(0:1, n),
+                                            labels = c("pos", "neg"))),
+                  y = data.frame(y = as.vector(rbind(as.numeric(diffs > 0),
+                                                     as.numeric(diffs < 0)))),
+                  block = gl(n, 2))
+
+    args <- setup_args(teststat = "scalar", paired = TRUE)
 
     object <- do.call("symmetry_test", c(list(object = object), args))
 
-    if (is_ordered_x(object@statistic))
-        object@method <- "Page Test"
-    else
-        object@method <- "Friedman Test"
+    object@method <- "Sign Test"
+    object@nullvalue <- 0
 
     return(object)
 }
@@ -157,57 +126,88 @@ wilcoxsign_test.SymmetryProblem <- function(object,
 }
 
 
-### sign test
-sign_test <- function(object, ...) UseMethod("sign_test")
+### Friedman Test
+friedman_test <- function(object, ...) UseMethod("friedman_test")
 
-sign_test.formula <- function(formula, data = list(), subset = NULL, ...)
-{
-    object <- formula2data(formula, data, subset, frame = parent.frame(), ...)
-    if (is.null(object$bl))
-        object <- list(y = data.frame(c(object$y[[1]], object$x[[1]])),
-                       x = data.frame(gl(2, length(object$x[[1]]))),
-                       block = factor(rep.int(1:length(object$x[[1]]), 2)))
-    object <- new("SymmetryProblem", x = object$x, y = object$y,
-                  block = object$bl)
-    object <- do.call("sign_test", c(list(object = object), list(...)))
-    return(object)
+friedman_test.formula <- function(formula, data = list(), subset = NULL, ...) {
+
+    ft("friedman_test", "SymmetryProblem", formula, data, subset,
+       frame = parent.frame(), ...)
 }
 
-sign_test.SymmetryProblem <- function(object, ...) {
+friedman_test.SymmetryProblem <- function(object, ...) {
 
-    y <- object@y[[1]]
-    x <- object@x[[1]]
-
-    if (!is.numeric(y))
-        stop(sQuote("y"), " is not a numeric variable")
-    if (is.factor(x)) {
-        if (nlevels(x) != 2)
-            stop(sQuote("x"), " is not a factor with two levels")
-        diffs <- tapply(1:length(y), object@block, function(b)
-            y[b][x[b] == levels(x)[1]] - y[b][x[b] == levels(x)[2]])
-    } else
-        stop(sQuote("x"), " is not a factor")
-
-    abs_diffs <- abs(diffs)
-    if (all(abs_diffs < .Machine$double.eps))
-        stop("all pairwise differences equal zero")
-
-    diffs <- diffs[abs_diffs > 0]
-    n <- length(diffs)
-
-    object <- new("SymmetryProblem",
-                  x = data.frame(x = factor(rep.int(0:1, n),
-                                            labels = c("pos", "neg"))),
-                  y = data.frame(y = as.vector(rbind(as.numeric(diffs > 0),
-                                                     as.numeric(diffs < 0)))),
-                  block = gl(n, 2))
-
-    args <- setup_args(teststat = "scalar", paired = TRUE)
+    args <- setup_args(ytrafo = function(data)
+                           trafo(data, numeric_trafo = rank_trafo,
+                                 block = object@block),
+                       check = function(object) {
+                           if (!(is_Ksample(object) && is_numeric_y(object)))
+                               stop(sQuote("object"),
+                                    " does not represent a K-sample problem",
+                                    " (maybe the grouping variable is not a",
+                                    " factor?)")
+                           return(TRUE)
+                       })
+    ## convert factors to ordered and attach scores if requested
+    if (!is.null(args$scores)) {
+        object <- setscores(object, args$scores)
+        args$scores <- NULL
+    }
+    ## set test statistic to scalar for linear-by-linear tests
+    args$teststat <- if (is_ordered_x(object)) "scalar"
+                     else "quadratic"
 
     object <- do.call("symmetry_test", c(list(object = object), args))
 
-    object@method <- "Sign Test"
-    object@nullvalue <- 0
+    if (is_ordered_x(object@statistic))
+        object@method <- "Page Test"
+    else
+        object@method <- "Friedman Test"
+
+    return(object)
+}
+
+
+### Quade Test
+quade_test <- function(object, ...) UseMethod("quade_test")
+
+quade_test.formula <- function(formula, data = list(), subset = NULL, ...) {
+
+    ft("quade_test", "SymmetryProblem", formula, data, subset,
+       frame = parent.frame(), ...)
+}
+
+quade_test.SymmetryProblem <- function(object, ...) {
+
+    args <- setup_args(check = function(object) {
+                           if (!(is_Ksample(object) && is_numeric_y(object)))
+                               stop(sQuote("object"),
+                                    " does not represent a K-sample problem",
+                                    " (maybe the grouping variable is not a",
+                                    " factor?)")
+                           return(TRUE)
+                       })
+    ## convert factors to ordered and attach scores if requested
+    if (!is.null(args$scores)) {
+        object <- setscores(object, args$scores)
+        args$scores <- NULL
+    }
+    ## set test statistic to scalar for linear-by-linear tests
+    args$teststat <- if (is_ordered_x(object)) "scalar"
+                     else "quadratic"
+
+    y <- split(object@y[[1]], object@block)
+    R <- lapply(y, function(y) rank(y) - (length(y) + 1) / 2)
+    Q <- rank(vapply(y, function(y) max(y) - min(y), NA_real_, USE.NAMES = FALSE))
+    object@y[[1]] <- unsplit(lapply(seq_along(Q), function(i) Q[i] * R[[i]]),
+                             object@block)
+
+    object <- do.call("symmetry_test", c(list(object = object), args))
+
+    if (is_ordered_x(object@statistic))
+        object@method <- "Linear-by-Linear Association Test"
+    else
+        object@method <- "Quade Test"
 
     return(object)
 }
