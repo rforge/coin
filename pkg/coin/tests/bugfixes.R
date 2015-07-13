e73e5d280bf168f40215ba1b36b8d4df3efa5650
hnilsson <hnilsson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-07-13 11:03:40 +0000

    tweak regression test + update reference output
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@956 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/coin/tests/bugfixes.R
+++ pkg/coin/tests/bugfixes.R
@@ -522,50 +522,18 @@ stopifnot(isequal(qperm(it, c(0.9, 0.95, 0.99)),
                   c(qperm(it, 0.9), qperm(it, 0.95), qperm(it, 0.99))))
 
 ### blockwise permutations were only correct for factors ordered wrt their levels
-## Ex. 1
-(y <- rep(1:4, 2))
+set.seed(36)
+.Call("R_blockperm", rep(1:4, 2),                       # was OK
+      PACKAGE = "coin")
 
 set.seed(36)
-.Call("R_blockperm", y)
-
-## Ex. 2
-(y <- rep(1:4, each = 2))
+.Call("R_blockperm", rep(1:4, each = 2),                # was OK
+      PACKAGE = "coin")
 
 set.seed(36)
-.Call("R_blockperm", y)
-
-## Ex. 3
-(y <- c(1:4, 4:1))
+.Call("R_blockperm", c(1:4, 4:1),                       # was OK
+      PACKAGE = "coin")
 
 set.seed(36)
-.Call("R_blockperm", y)
-
-## Ex. 4
-y <- c(4L, 1L, 2L, 2L, 4L, 3L, 1L, 3L)
-
-set.seed(36)
-.Call("R_blockperm", y)
-.Call("R_blockperm", sample(y))
-.Call("R_blockperm", sample(y))
-
-tst <- data.frame(
-    y = c(108, 56.5, 149, 173, 147, 147, 138, 167, 22.5, 151.5,
-          122.5, 167, 172, 147, 174, 156, 84, 132, 164.5, 128.5),
-    x = factor(c(2, 1, 2, 1, 1, 2, 2, 1, 1, 1,
-                 2, 2, 2, 2, 2, 1, 1, 2, 1, 1)),
-    b = factor(c(6L, 10L, 3L, 8L, 4L, 2L, 4L, 5L, 7L, 9L,
-                 1L, 7L, 9L, 8L, 10L, 2L, 6L, 5L, 3L, 1L))
-)
-
-set.seed(711109)
-independence_test(y ~ x | b, data = tst,
-                  distribution = approximate(B = 10000))
-independence_test(y ~ x | b, data = tst, distribution = "exact")
-independence_test(y ~ x | b, data = tst, distribution = "asym")
-
-tst2 <- tst[order(tst$b, tst$x), ]
-set.seed(711109)
-independence_test(y ~ x | b, data = tst2,
-                  distribution = approximate(B = 10000))
-independence_test(y ~ x | b, data = tst2, distribution = "exact")
-independence_test(y ~ x | b, data = tst2, distribution = "asym")
+.Call("R_blockperm", c(4L, 1L, 2L, 2L, 4L, 3L, 1L, 3L), # wrong
+      PACKAGE = "coin")
