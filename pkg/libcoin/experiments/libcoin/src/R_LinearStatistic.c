d9e9d297ee103a337779c3ab29e40e5b5a191109
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-11 17:33:13 +0000

    more interfaces
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1078 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/experiments/libcoin/src/R_LinearStatistic.c
+++ pkg/libcoin/experiments/libcoin/src/R_LinearStatistic.c
@@ -13,3 +13,39 @@ SEXP R_LinearStatistic(SEXP x, SEXP y) {
     UNPROTECT(1);
     return(ans);
 }
+
+SEXP R_LinearStatistic_weights(SEXP x, SEXP y, SEXP weights) {
+
+    SEXP ans;
+    
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    C_LinearStatistic_weights(REAL(x), NROW(x), NCOL(x), REAL(y), NCOL(y), 
+                              REAL(weights), REAL(ans));
+    UNPROTECT(1);
+    return(ans);
+}
+
+SEXP R_LinearStatistic_subset(SEXP x, SEXP y, SEXP subset) {
+
+    SEXP ans;
+    
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    C_LinearStatistic_subset(REAL(x), NROW(x), NCOL(x), REAL(y), NCOL(y), 
+                             INTEGER(subset), LENGTH(subset), REAL(ans));
+    UNPROTECT(1);
+    return(ans);
+}
+
+SEXP R_LinearStatistic_weights_subset(SEXP x, SEXP y, SEXP weights, SEXP subset) {
+
+    SEXP ans;
+    
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    C_LinearStatistic_weights_subset(REAL(x), NROW(x), NCOL(x), 
+                                     REAL(y), NCOL(y), REAL(weights), 
+                                     INTEGER(subset), LENGTH(subset), 
+                                     REAL(ans));
+    UNPROTECT(1);
+    return(ans);
+}
+
