b63eb9a66f28258945dc6b0f09da678efe039ab4
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-12 19:15:31 +0000

    remove set_zero
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1091 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/experiments/libcoin/src/Tables.c
+++ pkg/libcoin/experiments/libcoin/src/Tables.c
@@ -2,18 +2,10 @@
 #include "libcoin.h"
 #include "helpers.h"
 
-void C_2dtable_set_zero(int Nx, int Ny, int *NxNy_ans) {
-
-    for (int j = 0; j < Nx; j++) {
-         for (int k = 0; k < Ny; k++)
-              NxNy_ans[k * Nx + j] = 0;
-    }
-}
-
 /* table(ix, iy) */
 void C_2dtable(int *ix, int Nx, int *iy, int Ny, int N, int *NxNy_ans) {
 
-    C_2dtable_set_zero(Nx, Ny, NxNy_ans);
+    for (int i = 0; i < Nx * Ny; i++) NxNy_ans[i] = 0;
     
     for (int i = 0; i < N; i++)  
         NxNy_ans[ix[i] + iy[i] * Nx]++;
@@ -24,7 +16,7 @@ void C_2dtable(int *ix, int Nx, int *iy, int Ny, int N, int *NxNy_ans) {
 void C_2dtable_subset(int *ix, int Nx, int *iy, int Ny, int *subset, 
                       int Nsubset, int *NxNy_ans) {
 
-    C_2dtable_set_zero(Nx, Ny, NxNy_ans);
+    for (int i = 0; i < Nx * Ny; i++) NxNy_ans[i] = 0;
     
     for (int i = 0; i < Nsubset; i++)  
         NxNy_ans[ix[subset[i]] + iy[subset[i]] * Nx]++;
@@ -35,7 +27,7 @@ void C_2dtable_subset(int *ix, int Nx, int *iy, int Ny, int *subset,
 void C_2dtable_weights(int *ix, int Nx, int *iy, int Ny, int *weights,
                        int N, int *NxNy_ans) {
 
-    C_2dtable_set_zero(Nx, Ny, NxNy_ans);
+    for (int i = 0; i < Nx * Ny; i++) NxNy_ans[i] = 0;
     
     for (int i = 0; i < N; i++)
         NxNy_ans[ix[i] + iy[i] * Nx] += weights[i];
@@ -46,25 +38,22 @@ void C_2dtable_weights(int *ix, int Nx, int *iy, int Ny, int *weights,
 void C_2dtable_weights_subset(int *ix, int Nx, int *iy, int Ny, int *weights,
                               int *subset, int Nsubset, int *NxNy_ans) {
 
-    C_2dtable_set_zero(Nx, Ny, NxNy_ans);
+    for (int i = 0; i < Nx * Ny; i++) NxNy_ans[i] = 0;
     
     for (int i = 0; i < Nsubset; i++)
          NxNy_ans[ix[subset[i]] + iy[subset[i]] * Nx] += weights[subset[i]];
 }
 
-void C_1dtable_set_zero(int Ny, int *Ny_ans) {
-    for (int i = 0; i < Ny; i++) Ny_ans[i] = 0;
-}
-
 void C_1dtable(int *iy, int Ny, int N, int *Ny_ans) {
 
-    C_1dtable_set_zero(Ny, Ny_ans);
+    for (int i = 0; i < Ny; i++) Ny_ans[i] = 0;
+
     for (int i = 0; i < N; i++) Ny_ans[iy[i]]++;
 }
 
 void C_1dtable_subset(int *iy, int Ny, int *subset, int Nsubset, int *Ny_ans) {
 
-    C_1dtable_set_zero(Ny, Ny_ans);
+    for (int i = 0; i < Ny; i++) Ny_ans[i] = 0;
     
     for (int i = 0; i < Nsubset; i++)  
         Ny_ans[iy[subset[i]]]++;
@@ -72,7 +61,7 @@ void C_1dtable_subset(int *iy, int Ny, int *subset, int Nsubset, int *Ny_ans) {
 
 void C_1dtable_weights(int *iy, int Ny, int *weights, int N, int *Ny_ans) {
 
-    C_1dtable_set_zero(Ny, Ny_ans);
+    for (int i = 0; i < Ny; i++) Ny_ans[i] = 0;
     
     for (int i = 0; i < N; i++)  
         Ny_ans[iy[i]] += weights[i];
@@ -81,7 +70,7 @@ void C_1dtable_weights(int *iy, int Ny, int *weights, int N, int *Ny_ans) {
 void C_1dtable_weights_subset(int *iy, int Ny, int *weights, int *subset, 
                               int Nsubset, int *Ny_ans) {
 
-    C_1dtable_set_zero(Ny, Ny_ans);
+    for (int i = 0; i < Ny; i++) Ny_ans[i] = 0;
     
     for (int i = 0; i < Nsubset; i++)  
           Ny_ans[iy[subset[i]]] += weights[subset[i]];
