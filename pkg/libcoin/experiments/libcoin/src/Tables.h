e4aead80380cc8b5e5ff79aba68abfca0445ce4e
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-12 11:59:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1084 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/libcoin/experiments/libcoin/src/Tables.h
@@ -0,0 +1,15 @@
+
+void C_2dtable_set_zero(int Nx, int Ny, int *NxNy_ans);
+void C_2dtable(int *ix, int Nx, int *iy, int Ny, int N, int *NxNy_ans);
+void C_2dtable_subset(int *ix, int Nx, int *iy, int Ny, int *subset, 
+                      int Nsubset, int *NxNy_ans);
+void C_2dtable_weights(int *ix, int Nx, int *iy, int Ny, int *weights,
+                       int N, int *NxNy_ans);
+void C_2dtable_weights_subset(int *ix, int Nx, int *iy, int Ny, int *weights,
+                              int *subset, int Nsubset, int *NxNy_ans);
+void C_1dtable_set_zero(int Ny, int *Ny_ans);
+void C_1dtable(int *iy, int Ny, int N, int *Ny_ans);
+void C_1dtable_subset(int *iy, int Ny, int *subset, int Nsubset, int *Ny_ans);
+void C_1dtable_weights(int *iy, int Ny, int *weights, int N, int *Ny_ans);
+void C_1dtable_weights_subset(int *iy, int Ny, int *weights, int *subset, 
+                              int Nsubset, int *Ny_ans);
