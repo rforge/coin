e64960271b7589dfd68b8807ece36a6a1ae6b1ee
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-12 13:46:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1088 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/experiments/libcoin/R/libcoin.R
+++ pkg/libcoin/experiments/libcoin/R/libcoin.R
@@ -75,15 +75,15 @@ LinStatExpCov <- function(X, Y, weights, subset, block) {
            if (case == "vanilla")
                tmp <- LinStatExpCov(X, Y, subset = which(block == l))
            if (case == "weights")
-               tmp <- LinStatExpCov(X, Y, weights = weights, subset = which(block == l))$Covariance
+               tmp <- LinStatExpCov(X, Y, weights = weights, subset = which(block == l))
            if (case == "subset")
-               tmp <- LinStatExpCov(X, Y, subset = subset[which(block[subset] == l)])$Covariance
+               tmp <- LinStatExpCov(X, Y, subset = subset[which(block[subset] == l)])
            if (case == "weights_subset")
-               tmp <- LinStatExpCov(X, Y, weights = weights, subset = subset[which(block[subset] == l)])$Covariance
+               tmp <- LinStatExpCov(X, Y, weights = weights, subset = subset[which(block[subset] == l)])
            Cov <- Cov + tmp$Covariance
            Exp <- Exp + tmp$Expectation
        }
-       ret$Expecation <- Exp
+       ret$Expectation <- Exp
        ret$Covariance <- Cov
     } else {
         ret$Expectation <- .Call("R_ExpectationLinearStatistic", ExpY, ExpX)
@@ -104,61 +104,44 @@ w <- as.integer(floor(runif(n, max = 4)))
 s <- sample(1:n, floor(n/2), replace = TRUE)
 b <- sample(gl(2, 2, length = n))
 
-LinStatExpCov(X, Y)
-it <- independence_test(Y ~ X)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y, weights = w)
-it <- independence_test(Y ~ X, weights = ~ w)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y, subset = s)
-it <- independence_test(Y ~ X, subset = s)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y, weights = w, subset = s)
-it <- independence_test(Y ~ X, weights = ~w, subset = s)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y, block = b)
-it <- independence_test(Y ~ X  | b)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y)
-it <- independence_test(Y ~ X)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-
-if (FALSE) {
-
-LinStatExpCov(X, Y, weights = w, block = b)
-it <- independence_test(Y ~ X | b, weights = ~ w)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y, subset = s, block = b)
-it <- independence_test(Y ~ X | b, subset = s)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
-LinStatExpCov(X, Y, weights = w, subset = s, block = b)
-it <- independence_test(Y ~ X | b, weights = ~w, subset = s)
-statistic(it, "linear")
-expectation(it)
-covariance(it)
-
+cmp <- function(t1, t2) {
+    c(max(abs(t1$LinStat - statistic(t2, "linear"))),
+      max(abs(t1$Expectation - expectation(t2))),
+      max(abs(t1$Covariance - covariance(t2))))
 }
+
+t1 <-LinStatExpCov(X, Y)
+t2 <- independence_test(Y ~ X)
+cmp(t1, t2)
+
+t1 <-LinStatExpCov(X, Y, weights = w)
+t2 <- independence_test(Y ~ X, weights = ~ w)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y, subset = s)
+t2 <- independence_test(Y ~ X, subset = s)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y, weights = w, subset = s)
+t2 <- independence_test(Y ~ X, weights = ~w, subset = s)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y, block = b)
+t2 <- independence_test(Y ~ X  | b)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y)
+t2 <- independence_test(Y ~ X)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y, weights = w, block = b)
+t2 <- independence_test(Y ~ X | b, weights = ~ w)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y, subset = s, block = b)
+t2 <- independence_test(Y ~ X | b, subset = s)
+cmp(t1, t2)
+
+t1 <- LinStatExpCov(X, Y, weights = w, subset = s, block = b)
+t2 <- independence_test(Y ~ X | b, weights = ~w, subset = s)
+cmp(t1, t2)
