57559904f31f4b6f6dfee5aa3fae817d044cb4e8
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-12 13:12:04 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1086 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/experiments/libcoin/src/R_LinearStatistic.c
+++ pkg/libcoin/experiments/libcoin/src/R_LinearStatistic.c
@@ -10,7 +10,7 @@ SEXP R_LinearStatistic(SEXP x, SEXP y) {
 
     SEXP ans;
     
-    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(x), NCOL(y)));
     C_LinearStatistic(REAL(x), NROW(x), NCOL(x), REAL(y), NCOL(y), REAL(ans));
     UNPROTECT(1);
     return(ans);
@@ -20,7 +20,7 @@ SEXP R_LinearStatistic_weights(SEXP x, SEXP y, SEXP weights) {
 
     SEXP ans;
     
-    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(x), NCOL(y)));
     C_LinearStatistic_weights(REAL(x), NROW(x), NCOL(x), REAL(y), NCOL(y), 
                               INTEGER(weights), REAL(ans));
     UNPROTECT(1);
@@ -31,7 +31,7 @@ SEXP R_LinearStatistic_subset(SEXP x, SEXP y, SEXP subset) {
 
     SEXP ans;
     
-    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(x), NCOL(y)));
     C_LinearStatistic_subset(REAL(x), NROW(x), NCOL(x), REAL(y), NCOL(y), 
                              INTEGER(subset), LENGTH(subset), REAL(ans));
     UNPROTECT(1);
@@ -42,7 +42,7 @@ SEXP R_LinearStatistic_weights_subset(SEXP x, SEXP y, SEXP weights, SEXP subset)
 
     SEXP ans;
     
-    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(x), NCOL(y)));
     C_LinearStatistic_weights_subset(REAL(x), NROW(x), NCOL(x), 
                                      REAL(y), NCOL(y), INTEGER(weights), 
                                      INTEGER(subset), LENGTH(subset), 
@@ -55,7 +55,7 @@ SEXP R_LinearStatistic_2d(SEXP x, SEXP y, SEXP table) {
 
     SEXP ans;
     
-    PROTECT(ans = allocMatrix(REALSXP, NCOL(y), NCOL(x)));
+    PROTECT(ans = allocMatrix(REALSXP, NCOL(x), NCOL(y)));
     C_LinearStatistic_2d(REAL(x), NROW(x), NCOL(x), REAL(y), NROW(y), NCOL(y), 
                          INTEGER(table), REAL(ans));
     UNPROTECT(1);
