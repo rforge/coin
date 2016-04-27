cd2fcb2bbfa1b0b5a9bcb21dec3a1ed604d35c89
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-27 14:03:10 +0000

    implement MPinv for symmetric matrix in lower triangular packed form
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1106 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/experiments/libcoin/src/Utils.c
+++ pkg/libcoin/experiments/libcoin/src/Utils.c
@@ -120,3 +120,51 @@ void C_MPinv (SEXP x, double tol, SEXP s, SEXP u, SEXP v, double *MPinv,
         }
     }
 }
+
+/* MP inv of symmetric matrix in lower triangular packed form */
+
+SEXP R_MPinv_sym (SEXP x, SEXP tol) {
+
+    SEXP ans, rank, MP;
+    double *val, *vec, *dMP, dtol, *rx, *work, valinv;
+    int n, valzero = 0, info = 0, kn;
+
+    n = (int) (.5 + sqrt(.25 + 2 * LENGTH(x))) - 1;
+    
+    rx = Calloc(LENGTH(x), double);
+    Memcpy(rx, REAL(x), LENGTH(x));
+    work = Calloc(3 * n, double);
+    val = Calloc(n, double);
+    vec = Calloc(n * n, double);
+    
+    F77_CALL(dspev)("V", "L", &n, rx, val, vec, &n, work,
+                    &info);
+                                            
+    PROTECT(ans = allocVector(VECSXP, 2));
+    SET_VECTOR_ELT(ans, 0, MP = allocMatrix(REALSXP, n, n));
+    SET_VECTOR_ELT(ans, 1, rank = allocVector(INTSXP, 1));
+    dMP = REAL(MP);
+    
+    dtol = val[n - 1] * REAL(tol)[0];
+
+    for (int k = 0; k < n; k++)
+        valzero += (val[k] < dtol); 
+    INTEGER(rank)[0] = n - valzero;
+
+    for (int i = 0; i < n * n; i++) dMP[i] = 0.0;
+    
+    for (int k = valzero; k < n; k++) {
+        valinv = 1 / val[k];
+        kn = k * n;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                /* MP is symmetric */
+                dMP[j * n + i] += valinv * vec[kn + i] * vec[kn + j];
+                dMP[i * n + j] = dMP[j * n + i];
+            }
+        }
+    }
+    Free(rx); Free(work); Free(val); Free(vec);
+    UNPROTECT(1);
+    return(ans);
+}
--- pkg/libcoin/experiments/libcoin/tests/Utils.R
+++ pkg/libcoin/experiments/libcoin/tests/Utils.R
@@ -25,3 +25,14 @@ subset[block[subset] == 2] - 1
 .Call("R_PermuteBlock_subset", subset - 1L, block)
 gl(3, 5)
 .Call("R_PermuteBlock", gl(3, 5))
+
+
+set.seed(29)
+n <- 50
+X <- crossprod(matrix(runif(n^2), nr = n))
+x <- X[!upper.tri(X)]
+system.time(g1 <- partykit:::.MPinv(X))
+system.time(g2 <- .Call("R_MPinv_sym", x, sqrt(.Machine$double.eps)))
+names(g2) <- c("Xplus", "rank")
+all.equal(g1, g2)
+
