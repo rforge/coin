1ba2821872b8894768c270bdb8a838d4c8ef282e
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-29 13:19:44 +0000

    remove old stuff
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1109 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/experiments/libcoin/tests/Utils.R
+++ pkg/libcoin/experiments/libcoin/tests/Utils.R
@@ -2,18 +2,6 @@
 library("libcoin")
 
 set.seed(29)
-p <- 3
-x <- crossprod(matrix(runif(p * p), p))
-
-svd(x)
-
-.Call("R_svd", x)
-
-.Call("R_MPinv", x, sqrt(.Machine$double.eps))
-
-partykit:::.MPinv(x)
-
-set.seed(29)
 n <- 100
 k <- 20
 subset <- sample(1:n, k, replace = FALSE)
