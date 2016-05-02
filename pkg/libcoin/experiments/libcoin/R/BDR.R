2a079178f9a8d736f2c56d9d68491a8e3b0de10e
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-02 11:42:52 +0000

    experiment
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/libcoin/experiments/libcoin/R/BDR.R
@@ -0,0 +1,30 @@
+
+df2BDR <- function(object, max = 10) {
+
+    ret <- vector(mode = "list", length = ncol(object))
+    names(ret) <- colnames(object)
+
+    for (v in colnames(object)) {
+        x <- object[[v]]
+        if (!is.factor(x)) {
+            q <- unique(quantile(x, prob = 1:max / (max + 1)))
+            ind <- cut(x, breaks = c(-Inf, q, Inf))
+            ret[[v]] <- list(ind = unclass(ind),
+                             val = rbind(NA, cbind(c(-Inf, q), c(q, Inf))))
+        } else {
+            ret[[v]] <- list(ind = unclass(x),
+                             val = factor(c(NA, levels(x)), ordered = is.ordered(x)))
+        }
+        ret[[v]]$ind[is.na(x)] <- 0
+        attributes(ret[[v]]$ind) <- NULL
+    }
+    ret
+}
+
+IRIS <- iris[sample(1:nrow(iris), 1000000, replace = TRUE),]
+
+IRIS2 <- df2BDR(IRIS, 20)
+
+object.size(IRIS)
+object.size(IRIS2)
+
