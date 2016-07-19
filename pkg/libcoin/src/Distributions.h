7b27d43c37ed3f83ec2153495001fb10be7b3a8d
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-19 09:32:28 +0000

    add lower, give_log and alternative to maxtype distributions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1200 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/src/Distributions.h
+++ pkg/libcoin/src/Distributions.h
@@ -1,7 +1,8 @@
 
 double C_chisq_pvalue(double stat, int df, int lower, int give_log);
-double C_maxabsstat_pvalue(const double stat, const double *Covariance,
-    const int n, int maxpts, double releps, double abseps, double tol);
+double C_maxtype_pvalue(const double stat, const double *Covariance,
+    const int n, int alternative, int lower, int give_log,
+        int maxpts, double releps, double abseps, double tol);
 void C_Permute(int *x, int n, int *ans);
 void C_PermuteBlock(int *x, int *table, int Ntable, int *ans);
 void C_doPermuteBlock(int *subset, int Nsubset, int *table, int Nlevels, 
