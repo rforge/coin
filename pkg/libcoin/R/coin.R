dd8ac090671c37115a07bd37a91bc3e5b1856681
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-19 09:33:28 +0000

    no scaling, seed, names
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1203 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/R/coin.R
+++ pkg/libcoin/R/coin.R
@@ -5,13 +5,15 @@
                 Expectation = c(expectation(it)),
                 Covariance = c(covariance(it)[!upper.tri(covariance(it))]))
     names(ret$Expectation) <- NULL
-    names(ret$satistic) <- NULL
+    names(ret$statistic) <- NULL
+    names(ret$p.value) <- NULL
     ret
 }
 
 ### perl -pe 's/([a-z]*_test)\(/lc(\"\1\",/g;' < coin-Ex.R > tmp.R
 lc <- function(FUN, ...) {
 
+    set.seed(29)
     object <- do.call(FUN, list(...))
     blk <- object@statistic@block
     if (nlevels(blk) == 1) blk <- integer(0)
@@ -20,19 +22,28 @@ lc <- function(FUN, ...) {
     if (inherits(d, "AsymptNullDistribution")) B <- 0L
     w  <- as.integer(object@statistic@weights)
     s <- which(w > 0)
+    set.seed(29)
     lev <- LinStatExpCov(object@statistic@xtrans, 
                          object@statistic@ytrans, 
                          subset = s,
                          weights = w, 
                          block = blk, B = B)
     teststat <- "max"
-    if (inherits(object@statistic, "QuadTypeIndependenceTestStatistic")) 
+    alternative <- "two.sided"
+    if (inherits(object@statistic, "QuadTypeIndependenceTestStatistic")) {
         teststat <- "quad"
+    } else {
+        alternative <- object@statistic@alternative
+    }
         
-    tst <- Test(lev, xtrafo = "id", type = teststat)
+    tst <- Test(lev, xtrafo = "id", type = teststat, alternative = alternative)
     tst$LinearStatistic <- lev$LinearStatistic
+    if (length(tst$LinearStatistic) == 1 && 
+        alternative == "two.sided" && teststat != "quad") {
+        tst$statistic <- (lev$LinearStatistic - lev$Expectation) / sqrt(lev$Covariance)
+    }
     tst$Expectation <- lev$Expectation
     tst$Covariance <- lev$Covariance  
-    print(all.equal(tst, .testcoin(object)))
+    print(all.equal(tst, .testcoin(object), scale = 1))
     object
 }
