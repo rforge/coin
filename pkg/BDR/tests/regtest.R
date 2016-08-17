e1e835f879d9ccce7bd3e916c1159c218fab352c
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-17 11:35:09 +0000

    add checks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BDR/tests/regtest.R
+++ pkg/BDR/tests/regtest.R
@@ -5,11 +5,16 @@ data("iris")
 
 iris[3, "Sepal.Width"] <- NA
 
-iris1 <- BDR(iris, nmax = 5)
+iris1 <- BDR(iris, nmax = 5, as.interval = "Sepal.Width")
 
-iris2 <- BDR(iris, nmax = 5, total = TRUE)
+iris2 <- BDR(iris, nmax = 5, total = TRUE, as.interval = "Sepal.Width")
 
-all.equal(as.data.frame(iris1), as.data.frame(iris2))
+all.equal(x1 <- as.data.frame(iris1), x2 <- as.data.frame(iris2))
+
+table(x1$Species, iris$Species)
+
+tapply(iris$Sepal.Width, x1$Sepal.Width, range)
+levels(x1$Sepal.Width)
 
 (w <- weights(iris2))
 sum(w)
