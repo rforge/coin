63df132ca6565627a32cd037b717cc9b2b7ad8bc
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-17 10:28:05 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BDR/tests/regtest.R
+++ pkg/BDR/tests/regtest.R
@@ -9,17 +9,33 @@ iris1 <- BDR(iris, nmax = 5)
 
 iris2 <- BDR(iris, nmax = 5, total = TRUE)
 
-all.equal(as.data.frame(iris1), as.data.frame(iris1))
-
-iris$y <- Surv(runif(nrow(iris)), sample(c(TRUE, FALSE), nrow(iris), 
-                                         replace = TRUE))
-
-iris1 <- BDR(iris, nmax = 5)
-
-iris2 <- BDR(iris, nmax = 5, total = TRUE)
-
-all.equal(as.data.frame(iris1), as.data.frame(iris1))
+all.equal(as.data.frame(iris1), as.data.frame(iris2))
 
 (w <- weights(iris2))
 sum(w)
+
+x <- runif(100)
+x[1:3] <- NA   
+ix <- interval(x, breaks = 0:10/10)
+
+levels(ix)
+nlevels(ix)
+ix
+  
+table(ix)
+ix[1:10]
+
+enum(gl(3, 3))
+enum(gl(3, 3, ordered = TRUE))
+enum(c(TRUE, FALSE))
+enum(c(1:3, 20L, 30L))
+
+x <- sample(c(1:3, 10L, 20L), 100, replace = TRUE)
+x[1:3] <- NA
+ix <- enum(x)
+levels(ix)   
+nlevels(ix)  
+ix
+  
+table(ix)
 
