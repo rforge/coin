1fdc5f120c5beabacd625c9373db39e04d76cfb8
hnilsson <hnilsson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-02-09 14:57:26 +0000

    port r1434 from devel
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1435 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/coin/R/zAccess.R
+++ pkg/coin/R/zAccess.R
@@ -209,17 +209,17 @@ setGeneric("rperm", function(object, n, ...)
     standardGeneric("rperm"))
 
 setMethod("rperm",
-          signature = "NullDistribution",
-          definition = function(object, n, ...) {
-              qperm(object, runif(n))
-          }
+    signature = "NullDistribution",
+    definition = function(object, n, ...) {
+        qperm(object, runif(n))
+    }
 )
 
 setMethod("rperm",
-          signature = "IndependenceTest",
-          definition = function(object, n, ...) {
-              qperm(object, runif(n))
-          }
+    signature = "IndependenceTest",
+    definition = function(object, n, ...) {
+        qperm(object, runif(n))
+    }
 )
 
 
