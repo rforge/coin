e21c8f089edbd6f5e647b31c638f424c4887df77
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-11-28 14:09:13 +0000

    remove
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1632 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/libcoin/inst/C_API_example/R/ExpectationCovarianceStatistic.R
+++
@@ -1,6 +0,0 @@
-
-ExpectationCovarianceStatistic <- function(x, y) {
-    .Call(R_ExpectationCovarianceStatistic,
-          x, y, integer(0), integer(0), integer(0), 0L,
-          sqrt(.Machine$double.eps))
-}
