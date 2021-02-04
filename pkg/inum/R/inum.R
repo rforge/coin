7fc1d15cfed77438f561d4196f83e088ffe3ffb2
thothorn <thothorn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-02-04 14:27:09 +0000

    rownames
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@1999 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inum/R/inum.R
+++ pkg/inum/R/inum.R
@@ -57,6 +57,7 @@ inum.data.frame <- function(object, nmax = 20, ignore = NULL, total = FALSE,
             cc[is.na(cc)] <- TRUE
             if (any(!cc)) {
                 sDF <- sDF[cc,,drop = FALSE]
+                rownames(sDF) <- 1:nrow(sDF)
                 ret <- ret - 1L
             }
         }  
